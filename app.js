75c457da2ac5c8eeb8a3a8a79e9a81eddc8b10e6
Wwright91 <wisdomwright-martin@pursuit.org>
2024-02-12 15:07:11 -0500

    Health check route complete
    1. Required cors in app.js
    2. Implemented route for health check

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 // TODO: Add application-wide middleware
+const cors = require("cors");
 app.use(cors());
 
 // TODO: Add controller(s)
 
 // TODO: Implement health check route
+app.use("/", (req, res) => {
+  res.send("Health Check Complete");
+});
 
 module.exports = app;
