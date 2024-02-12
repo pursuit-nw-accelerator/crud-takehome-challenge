5c6fdfcf4c2c8751defa4c8babc9aa82c48ce2e4
Wwright91 <wisdomwright-martin@pursuit.org>
2024-02-12 17:59:35 -0500

    Created route for specific job 1. Wrote helper function for validating a given id 2. Route for /jobs/:id created 3. User is now able to see a job of a specific id

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,13 +1,48 @@
 const { Router } = require("express");
 
-const { getAllApplications } = require("../queries/jobApplicationsQueries");
+const {
+  getAllApplications,
+  getApplicationById,
+} = require("../queries/jobApplicationsQueries");
 
 const jobsController = Router();
+
+const validateId = (id, res) => {
+  let regex = /^[1-9]\d*$/;
+  if (regex.test(id)) {
+    return true;
+  } else {
+    res
+      .status(400)
+      .json({ error: `Id param must be a postive integer! Received: ${id}` });
+    return false;
+  }
+};
 
 jobsController.get("/", async (req, res) => {
   try {
     const jobs = await getAllApplications();
     res.status(200).json({ data: jobs });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+jobsController.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!validateId(id, res)) {
+      return;
+    }
+
+    const job = await getApplicationById(Number(id));
+
+    if (job) {
+      res.status(200).json({ data: job });
+    } else {
+      return res.status(404).json({ error: `No job found with id: ${id}` });
+    }
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
