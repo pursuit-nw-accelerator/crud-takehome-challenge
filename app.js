06f820d19ad315722462bf4618c3e0ec609fc877
Allahvel Salisbury <allahvelsalisbury@pursuit.org>
2024-02-13 16:36:52 -0500

    Successfuly create controllers and routes. Also applied proper middleware.

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+const controllers = require('./controllers/jobsController')
+
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 // TODO: Add application-wide middleware
+app.use(express.json())
 app.use(cors());
 
 // TODO: Add controller(s)
+app.get('/applications', controllers.getJobApplications)
+app.get('/applications:id', controllers.getJobApplicationById)
+app.post('/applications', controllers.createJobApplication)
+app.put('/applications:id', controllers.updateJobApplication)
+app.delete('/applications:id', controllers.deleteJobApplication)
 
 // TODO: Implement health check route
+app.get('/health', (req, res) => {
+    res.send('Health Check OK');
+  });
 
 module.exports = app;
---
+++ controllers/jobsController.js
@@ -0,0 +1,86 @@
+//controller to handle job apps
+const jobApplicationsQueries = require('../queries/jobApplicationsQueries');
+
+
+  //function to GET
+
+const getJobApplications = async (req,res) => {
+    try {
+        const applications = await jobApplicationsQueries.getAllApplications(); 
+        //if successful
+        res.status(200).json(applications)
+    } catch (error) {
+        //if error
+        console.log('Error fecthing applications:', error)
+        res.status(500).json({error: 'Internal Server Error'})
+    }
+};
+
+const getJobApplicationById = async (req,res) => {
+    //id must be integer
+    const id = parseInt(req.params.id);
+
+    try {
+        const application = await jobApplicationsQueries.getApplicationById(id);
+        //if id isnt found return 404 otherwise return id
+        if(!application){
+            return res.status(404).json({error: 'Applications not found'})
+        } 
+        res.status(200).json(application)
+        //catch other errors
+    } catch (error) {
+        console.log('Error getting ID:', error)
+        res.status(500).json({error: 'Internal Server Error'})
+    }
+
+};
+
+const createJobApplication = async (req,res) => {
+    const applicationData = req.body
+    try {
+        const newApplication = await jobApplicationsQueries.createApplication(applicationData)
+        //if successfully created
+        res.status(201).json(newApplication)
+    } catch (error) {
+        console.log('Error created app', error)
+        res.status(500).json('Internal Server Error')
+    }
+};
+
+const updateJobApplication = async (req,res) => {
+    const id = parseInt(req.params.id);
+    const updatedData = req.body
+    try {
+        const updatedApp = await jobApplicationsQueries.updateApplication(id, updatedData)
+        if(!updatedApp) {
+            res.status(404).json({error: 'Could not find App'})
+        }
+        res.status(200).json(updatedApp)
+    } catch ( error ) {
+        console.log('Error updating ID', error)
+    res.status(500).json('Internal Server Error')
+    }
+};
+
+const deleteJobApplication = async (req, res) => {
+    const id = parseInt(req.params.id);
+    // const deleteData = req.body
+    try {
+        const deleteApp = await jobApplicationsQueries.deleteApplication(id)
+        if(!deleteApp) {
+            res.status(404).json('Could not find App')
+        } 
+        res.status(200).json(deleteApp)
+    } catch (error) {
+        console.log('Error deleting app:', error)
+        res.status(500).json('Internal Server Error')
+    }
+};
+
+module.exports = {
+    getJobApplications,
+    getJobApplicationById,
+    createJobApplication,
+    updateJobApplication,
+    deleteJobApplication
+}
