16fdda1794c7eeb03630de70960dbb13ebab23b7
antunishdPursuit <dennysantunish@pursuit.org>
2024-02-14 16:57:52 -0500

    added get route for a job application by id

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -19,4 +19,26 @@ jobsController.get('/', async(req, res) => {
     }
 })
 
+jobsController.get('/:id', async(req, res) => {
+    try {
+        const { id } = req.params
+        //Make a validation route for below 
+        if (!Number.isInteger(Number(id))) {
+            // the id is not valid
+            return res.status(400).json({ error: `Invalid id: ${id}` });
+        }
+        const job = await getApplicationById(Number(id))
+        if (job) {
+            res
+            .status(200).json({ data: job });
+        } else {
+            return res
+            .status(404)
+            .json({ error: `Job Application doesn't exist for id ${id}` });
+          }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
 module.exports = jobsController;
