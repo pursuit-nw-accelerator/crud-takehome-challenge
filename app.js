001889efc4407f92a8bbcae29fcba81ceadf1ed3
crsvalle <cristian.valle@pursuit.org>
2024-02-17 18:31:39 -0500

    init file, health check implemented

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
 
 // TODO: Add application-wide middleware
 app.use(cors());
+app.use(express.json())
 
 // TODO: Add controller(s)
 
 // TODO: Implement health check route
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Server working!"})
+})
+
+app.get('*', (req, res) => {
+    res.status(404).json({message: `path name ${req.url} does not exist`})
+})
 
 module.exports = app;
