8802ae0903ba838b21fe840fc63a20798a17bbd2
crsvalle <cristian.valle@pursuit.org>
2024-02-17 19:17:40 -0500

    added getAll, getID, deletebyId

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -3,8 +3,45 @@ const jobs = express.Router();
 
 const { getAllApplications, getApplicationById, createApplication, updateApplication, deleteApplication, } = require('../queries/jobApplicationsQueries')
 
-jobs.get('/', async(req,res) => {})
-jobs.get('/:id', async(req,res) => {})
-jobs.post('/', async(req,res) => {})
-jobs.delete('/:id', async(req,res) => {})
-jobs.put('/:id', async (req,res) => {})
+jobs.get('/', async (req, res) => {
+    try{
+        const allJobs = await getAllApplications()
+        if (allJobs[0]) {
+            res.status(200).json({ data: allJobs })
+        } else {
+            res.status(404).json({ error: "No jobs found" })
+        }
+    }catch(error){
+        res.status(500).json({error: "Server Error"})
+    }
+})
+jobs.get('/:id', async (req, res) => { 
+    const { id } = req.params;
+    try{
+        const oneJob = await getApplicationById(Number(id));
+        if (oneJob){
+            res.status(200).json({data: oneJob})
+        }else{
+            res.status(404).json({error: "Job not found"})
+        }
+    }catch(error){
+        res.status(500).json({error: "Server Error"})
+    }
+})
+jobs.post('/', async (req, res) => { })
+
+jobs.delete('/:id', async (req, res) => { 
+    const { id } = req.params;
+    try{
+        const deleteJob = await deleteApplication(Number(id))
+        if(!deleteJob){
+            res.status(404).json({error: "Could not delete because it was not found"})
+        }
+        res.status(200).json({data: deleteJob})
+    }catch(error){
+        res.status(500).json({error: "Server Error"})
+    }
+})
+jobs.put('/:id', async (req, res) => { })
+
+module.exports = jobs
