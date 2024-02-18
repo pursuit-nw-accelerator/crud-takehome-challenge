9e84f1ad2b76db2be33c91cca691f7ef3ef92d0a
RK-404 <rizwan_k@RIZWANs-Air.lan>
2024-02-18 09:43:32 -0500

    Added missing Middleware

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const cors = require('cors');
 const express = require('express');
-const applications = require("./controllers/applicationController");
+const items = require("./controllers/applicationController");
 
 const app = express();
 
 // TODO: Add application-wide middleware
 app.use(cors());
+app.use(express.json());
 
 // TODO: Add controller(s)
-app.use('/applications', applications);
+app.use('/applications', items);
 
 // TODO: Implement health check route
 app.get('/', (req, res) => {
     res.status(200).json({ data: 'Service is running'});
+});
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
 });
 
 module.exports = app;
