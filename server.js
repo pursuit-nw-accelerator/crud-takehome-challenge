1b993ba44483be30f8679050e4981b2edda3d301
Sabri Mohiuddin <you@example.com>
2024-02-17 20:30:00 -0500

    implement .env file

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
+
 const app = require("./app");
 
-// TODO: Read the port from environment variables,
-// or use 9000 as a default
-const PORT = 9000;
+// TODO: Read the port from environment variables, or use 9000 as a default
+// const PORT = 9000;
+// Other requirements 7
+const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, console.log(`Listening on port ${PORT}...`));
+//  'console.log' is executed as soon as this line is read, not after the server successfully starts
+// app.listen(PORT, console.log(`Listening on port ${PORT}...`));
+
+// uses an arrow function as a callback that executes after the server successfully starts listening on the specified port.
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
