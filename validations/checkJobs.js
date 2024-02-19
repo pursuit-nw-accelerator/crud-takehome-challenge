f2cb14cbbc9f8fec2c5dcf6a8de6770b5457f29c
crsvalle <cristian.valle@pursuit.org>
2024-02-18 20:37:50 -0500

    reformatted code

--- validations/checkJobs.js
+++ validations/checkJobs.js
@@ -10,33 +10,33 @@ const validateId = (req, res, next) => {
     next();
 }
 
-    const validateApp = (req, res, next) => {
-        const jobApp  = req.body;
+const validateApp = (req, res, next) => {
+    const jobApp = req.body;
 
-        for (const field of validFields){
-            if(!jobApp.hasOwnProperty(field)){
-                return res.status(400).json({
-                    error: `${field} is missing or wrong, received ${jobApp[field]}`
-                })
-            }
+    for (const field of validFields) {
+        if (!jobApp.hasOwnProperty(field)) {
+            return res.status(400).json({
+                error: `${field} is missing or wrong, received ${jobApp[field]}`
+            })
         }
-        if (typeof jobApp['company'] !== 'string'){
-            return res.status(400).json({error: `Something is wrong with ${jobApp['company']}`})
-        }
-        if (typeof jobApp['status'] !== 'string' || !Object.values(applicationStatuses).includes(jobApp['status'].toUpperCase())){
-            return res.status(400).json({error: `Status ${jobApp['status']} is not available`})
-        }
-        
-        for (const field in jobApp){
-            if (!validFields.includes(field)){
-                return res.status(400).json({error: `${field} is not allowed.`})
-            }
-        }
-        if(!jobApp['url']){
-            jobApp['url'] = null
-        }
-
-        next()
     }
+    if (typeof jobApp['company'] !== 'string') {
+        return res.status(400).json({ error: `Something is wrong with ${jobApp['company']}` })
+    }
+    if (typeof jobApp['status'] !== 'string' || !applicationStatuses[jobApp['status'].toUpperCase()]) {
+        return res.status(400).json({ error: `Status ${jobApp['status']} is not available` })
+    }
+
+    for (const field in jobApp) {
+        if (!validFields.includes(field)) {
+            return res.status(400).json({ error: `${field} is not allowed.` })
+        }
+    }
+    if (!jobApp['url']) {
+        jobApp['url'] = null
+    }
+
+    next()
+}
 
 module.exports = { validateId, validateApp }
