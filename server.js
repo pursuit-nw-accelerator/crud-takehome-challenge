e12e3000464f03408468a2bed566d2f337bbad07
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-02-27 10:39:59 -0500

    added require dotenv w config

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('./app');
 
+//dotenv config()
+const dotenv = require('dotenv');
+dotenv.config({ path: "./config.env" });
+
 // TODO: Read the port from environment variables,
 // or use 9000 as a default
 const PORT = process.env.PORT || 9000;
