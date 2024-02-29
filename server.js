d3c6a4aabea925d304339a92c52d0dbff4c295fd
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-02-29 11:58:57 -0500

    added dotenv config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 // TODO: Read the port from environment variables,
 // or use 9000 as a default
-//dotenv config()
+// https://medium.com/@akhilanand.ak01/simplify-your-node-js-configuration-with-dotenv-env-ee371ad6bf9a
 const dotenv = require('dotenv');
 dotenv.config({ path: "./config.env" });
 
