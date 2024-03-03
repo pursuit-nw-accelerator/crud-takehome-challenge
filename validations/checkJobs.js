0d9b7dac44ab0dd725a57ca55421517eaba77777
crsvalle <cristian.valle@pursuit.org>
2024-03-02 20:17:13 -0500

    made sure id validation has to a positive int

--- validations/checkJobs.js
+++ validations/checkJobs.js
@@ -3,7 +3,7 @@ const validFields = ['company', 'status', 'url']
 
 const validateId = (req, res, next) => {
     const { id } = req.params;
-    if (!Number.isInteger(Number(id))) {
+    if (!Number.isInteger(Number(id)) || Number(id) < 1) {
         return res.status(400).json({ error: 'id is not valid' })
     }
 
