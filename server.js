6e74f412a85ca48c181cec7f7b6f443bd03f23b8
Shanice <shanicegriffin@pursuit.org>
2024-05-19 15:39:37 -0400

    added .env file and changed PORT

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('./app');
 
 // TODO: Read the port from environment variables,
 // or use 9000 as a default
+require("dotenv").config()
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, console.log(`Listening on port ${PORT}...`));
