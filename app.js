a89652e8d359ef5bf1ecd5ae8d122d9706223e28
Alexander Tsiklidis <alexandertsiklidis@pursuit.org>
2024-05-20 16:31:13 -0400

    completes backend setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { validateApplication, validateId } = require('./middleware/validation');
+const { validateApplication, validateId } = require('./middlewares/validations');
 const {
     getAll,
     getById,
