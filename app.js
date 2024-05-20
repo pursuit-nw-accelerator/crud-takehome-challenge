caf7400a4eb27ad02e04db6515d70c341cc71c6d
Alexander Tsiklidis <alexandertsiklidis@pursuit.org>
2024-05-20 16:28:44 -0400

    completes app.js

--- app.js
+++ app.js
@@ -16,7 +16,20 @@ app.use(cors());
 app.use(express.json());
 
 // TODO: Add controller(s)
+app.get('/applications', getAll);
+app.get('/applications/:id', validateId, getById);
+app.post('/applications', validateApplication, create);
+app.put('/applications/:id', validateId, validateApplication, update);
+app.delete('/applications/:id', validateId, remove);
 
 // TODO: Implement health check route
+app.get('/', (req, res) => {
+    res.status(200).json({ data: "Service is running" });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ error: err.message });
+});
 
 module.exports = app;
